110a21c7dda62b49d57c43b3005e26c86360d03c
matheusoliv23 <matheushenri1996@gmail.com>
2022-02-14 16:28:04 -0300

    Resolvendo erro de build

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,8 +1,12 @@
 import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
+const baseUrl = `${
+  process.env.PRISMIC_API_ENDPOINT || 'http://localhost:8080'
+}/`;
+
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
+  const prismic = Prismic.client(baseUrl, {
     req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN
   });
