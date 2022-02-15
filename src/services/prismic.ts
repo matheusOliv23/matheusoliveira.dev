f20d84bca14fa340bcd80e44a9837ea50f28f1d4
matheusoliv23 <matheushenri1996@gmail.com>
2022-02-15 12:31:36 -0300

    Erros deploy

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,10 +1,6 @@
 import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
 
-const baseUrl = `${
-  process.env.PRISMIC_API_ENDPOINT || 'http://localhost:3000'
-}`;
-
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
     req,
